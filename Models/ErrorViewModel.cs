87d24a4f00c4b928fc7630adcaf2d40248e03d30
Romario Arnat <Romario.Arnat@green.com.na>
2024-02-21 16:26:37 +0200

    ddddddddddddddddddddd

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace TakeALot.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
